4ec01c5bb9bf6e32fff73ea908a3890ebdbc78af
liuke <xcslk@qq.com>
2017-08-22 17:34:34 +0800

    更改了部分方法的名称； 更改了没有复用的部分方法的位置；

--- util.py
+++ util.py
@@ -1,15 +1,13 @@
-import json
 import requests
 import time
-import tablib
 from requests import RequestException
 from retrying import retry
 
-from constant import city_center_base_url, ak, timeout, steps, distance_unit, headers
+from constant import city_center_url_pattern, ak, timeout, steps, distance_unit, headers
 
 
-def get_city_center(city_name):
-    city_url = city_center_base_url.format(city_name, ak)
+def get_city_center_location_with_city(city_name):
+    city_url = city_center_url_pattern.format(city_name, ak)
     try:
         response = requests.get(city_url, timeout=timeout)
         response_dict = response.json()
@@ -49,8 +47,8 @@ def get_rects_by_center(location):
     return rect_list
 
 
-def get_rect_list(city_name):
-    location = get_city_center(city_name)
+def get_rect_list_with_city(city_name):
+    location = get_city_center_location_with_city(city_name)
     rect_list = get_rects_by_center(location)
     return rect_list
 
@@ -67,47 +65,17 @@ def get_response_text_with_url(url):
         return None
 
 
-def get_anjuke_2nd_community_info(html):
-    if html:
-        content = json.loads(html)
-        if content and 'val' in content:
-            text = content['val']['comms']
-            return text
-    return None
-
-
-# TODO city_name 需要转换成全拼
-def get_anjuke_2nd_community_url(city_name, rect):
-    format_data = [city_name] + rect
-    base_url = 'https://{}.anjuke.com/v3/ajax/map/sale/facet/?room_num=-1&price_id=-1&area_id=-1&floor=-1&orientation=-1&is_two_years=0&is_school=0&is_metro=0&order_id=0&p=1&zoom=19&' \
-               'lat={}_{}&lng={}_{}&kw=&maxp=99'
-    url = base_url.format(*format_data)
-    return url
-
-
 def get_date():
     date = time.strftime("%Y_%m_%d", time.localtime())
     return date
 
 
-def get_tsv_formed_data(community, source, service_life):
-    if source == '安居客' and service_life == '二手':
-        pre_data = [community['truncate_name'],
-                    '%-.6f' % float(community['lat']),
-                    '%-.6f' % float(community['lng']),
-                    '%-s' % community['address'],
-                    '%-s' % community['mid_price'],
-                    '%-s' % community['id'],
-                    '%-s' % community['prop_num']]
-        if community['mid_change']:
-            pre_data.append('%-.3f' % (float(community['mid_change'])))
-        data = tablib.Dataset(*pre_data)
-        return data
-
-
-def save_and_write_info(city_name, source, service_life, data):
+def get_raw_data_file_path(city_name, source_name, service_life):
     date = get_date()
-    with open('{}_{}_{}_{}'.format(city_name, source, service_life, date), 'a', encoding='utf-8') as file:
+    file_path = '{}_{}_{}_{}.tsv'.format(city_name, source_name, service_life, date)
+    return file_path
+
+
+def save_raw_data_in_tsv_file(file_path, data):
+    with open(file_path, 'a', encoding='utf-8') as file:
         file.write(data.tsv)
-
-
